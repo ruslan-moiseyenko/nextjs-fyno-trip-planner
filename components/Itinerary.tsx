0457318d2059a663c76ffa9b89405b9d0619d1ef
Ruslan Moiseyenko <ruscom5@gmail.com>
2024-06-09 11:26:00 +0300

    mend

--- components/Itinerary.tsx
+++ components/Itinerary.tsx
@@ -7,16 +7,19 @@ import person from "@/assets/svg/person.svg";
 
 export const Itinerary = () => {
   return (
-    <div className="border-border_blue bg-background_secondary flex w-full gap-3 rounded-3xl border-[1px] px-4 py-6">
-      <InputWithIcon placeholder="Reykjavík, KEF" />
-      <Image src={airplane} alt="airplane" />
-      <InputWithIcon placeholder="San Francisco, SFO" />
-      <InputWithIcon
-        placeholder="Jul 03 – Jul 11"
-        icon={calendar}
-        width="w-[190px]"
-      />
-      <InputWithIcon placeholder="2" icon={person} width="w-[70px]" />
-    </div>
+    <section className="py-6">
+      <h2 className="mb-6 text-2xl font-semibold">Iceland itinerary</h2>
+      <div className="border-border_blue bg-background_secondary flex w-full gap-3 rounded-3xl border-[1px] px-4 py-6">
+        <InputWithIcon placeholder="Reykjavík, KEF" />
+        <Image src={airplane} alt="airplane" />
+        <InputWithIcon placeholder="San Francisco, SFO" />
+        <InputWithIcon
+          placeholder="Jul 03 – Jul 11"
+          icon={calendar}
+          width="w-[190px]"
+        />
+        <InputWithIcon placeholder="2" icon={person} width="w-[70px]" />
+      </div>
+    </section>
   );
 };
